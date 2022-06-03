b999f73aa63e56ad8040dc370d1e644e7e58fb92
jmshanahan <comeraghsolutions@gmail.com>
2022-06-03 11:06:15 +0100

    Flexbox issue fixed

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Gardalocation
 A simple garda location application using esri development environment
+
+
+
+# Unresolved Issues
+
+## Nesting flex box
+I wanted to nest the MapViwe inside a flexbox container so I could another column to the right of it. But when I did The map would not display.
+
+This would work
+```
+        <div>
+            <div id="viewDiv"></div>
+        </div>
+```
+But it needed the ```getElementById````
+```
+    const viewNode = window.document.getElementById("viewDiv");
+    
+    
+    const view = new MapView({
+      container: viewNode, // Reference to the view div created in step 5
+      map: map, // Reference to the map object created before the view
+      zoom: 10, // Sets zoom level based on level of detail (LOD)
+      center: [-7, 53] // Sets center point of view using longitude,latitude
+    });
+
+```
+This would not work
+
+```
+    const view = new MapView({
+      container: "viewDiv", // Reference to the view div created in step 5
+      map: map, // Reference to the map object created before the view
+      zoom: 10, // Sets zoom level based on level of detail (LOD)
+      center: [-7, 53] // Sets center point of view using longitude,latitude
+    });
+
+```
+The only way this would work was to remove the surrounding ```div``` from the ```MapView'```
+
+The dom would look like this
+```
+    <div id="viewDiv"></div>
+ ```
--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,20 @@ window.addEventListener('load',checkJSLoaded)
 
 function checkJSLoaded() {
 
-require(["esri/config", "esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/renderers/SimpleRenderer"], function (esriConfig, Map, MapView,FeatureLayer, SimpleRenderer) {
+require(["esri/config", 
+"esri/Map", 
+"esri/views/MapView",
+"esri/layers/FeatureLayer",
+"esri/renderers/SimpleRenderer",
+"esri/PopupTemplate",
+"esri/popup/content/CustomContent",
+], function (esriConfig, 
+  Map, 
+  MapView,
+  FeatureLayer,
+  PopupTemplate,
+  CustomContent, 
+  SimpleRenderer) {
     esriConfig.apiKey = "AAPK340d910693ae44d888a6eb2e2b3224fcx9GYb-Ur-SC4Ubg-kT1zoSv0Y2H5Jb6qQ367krBx-Olc42mFGCzl9LzNO2V_MLdV";
     const gardaRenderer = {
     "type": "simple",
@@ -36,6 +49,7 @@ require(["esri/config", "esri/Map", "esri/views/MapView","esri/layers/FeatureLay
       expression: "$feature.COUNTY"
     }
   };
+  
 
   const popupGarda = {
     "title": "Station",
@@ -49,15 +63,18 @@ require(["esri/config", "esri/Map", "esri/views/MapView","esri/layers/FeatureLay
     outFields: ["STATION_NA","DIVISION_H"],
     popupTemplate: popupGarda
     });
+  
+
     const map = new Map({
       basemap: "arcgis-streets-relief",
       layers:[fl]
     });
-    // map.add(fl);  // adds the layer to the map
-    let viewNode = document.getElementById("viewDiv");
-    // console.log("log node");
+    
+    const viewNode = window.document.getElementById("viewDiv");
+    
+    
     const view = new MapView({
-      container: "viewDiv", // Reference to the view div created in step 5
+      container: viewNode, // Reference to the view div created in step 5
       map: map, // Reference to the map object created before the view
       zoom: 10, // Sets zoom level based on level of detail (LOD)
       center: [-7, 53] // Sets center point of view using longitude,latitude
